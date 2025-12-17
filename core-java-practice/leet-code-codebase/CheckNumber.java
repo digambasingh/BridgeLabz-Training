5dbbbeaf87fa399fa49a7c81c79010dfe1e47466
Digambar singh <digambar.singh_cs22@gla.ac.in>
2025-12-17 16:05:57 +0530

    [Digambar Singh] remaing four leetcode problem

---
+++ core-java-practice/leet-code-codebase/CheckNumber.java
@@ -0,0 +1,13 @@
+public class CheckNumber {
+    public static void main(String[] args) {
+        int number = -5;
+
+        if (number > 0) {
+            System.out.println("Positive number");
+        } else if (number < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
---
+++ core-java-practice/leet-code-codebase/SumTwoNumbers.java
@@ -0,0 +1,11 @@
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 7;
+
+        int sum = x + y;
+
+        System.out.println("Sum is: " + sum);
+    }
+}

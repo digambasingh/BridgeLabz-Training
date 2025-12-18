614350c4c7bdcbcdf5f57cc7167b73859e9f19d6
Digambar singh <digambar.singh_cs22@gla.ac.in>
2025-12-18 16:17:30 +0530

    [Digambar singh] level2

---
+++ core-java-practice/gcr-codebase/Google-ClassRoom-Questions/programmingElement/AreaOfTriangle.java
@@ -0,0 +1,15 @@
+package com.bridge.lab.day2.level2;
+
+public class AreaOfTriangle {
+
+	public static void main(String[] args) {
+		// Write a program that takes the base and height in cm to find the area of a triangle in square inches and square centimeters 
+		// Hint => Area of a Triangle is ½ * base * height and 1 in = 2.54 cm
+		int base = 12;
+		int height = 15;
+		double area = 0.5*base*height;
+		System.out.println("Area of Trainglein sq is: "+area);
+
+	}
+
+}
---
+++ core-java-practice/gcr-codebase/Google-ClassRoom-Questions/programmingElement/DoubleOpeariton.java
@@ -0,0 +1,17 @@
+package com.bridge.lab.day2.level2;
+
+public class DoubleOpeariton {
+
+	public static void main(String[] args) {
+		 
+		double a = 20;
+		double b = 10;
+		double c = 30;
+		double op1 = a+b*c;
+		double op2 = a*b+c;
+		double op3 = c+a/b;
+		double op4 = a%b+c;
+		System.out.println("The operation1: "+op1+" Operation2: "+op2+ " Opeariotn3: "+op3+" Opeariotn4: "+op4);
+	}
+
+}

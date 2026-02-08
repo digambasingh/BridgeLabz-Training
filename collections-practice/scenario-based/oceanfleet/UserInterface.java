cc0496e1bd4b115ad937bf617672e8486f004448
Digambar singh <digambar.singh_cs22@gla.ac.in>
2026-02-08 15:03:59 +0530

    [Digambar singh] add. ocean problem

---
+++ collections-practice/scenario-based/oceanfleet/UserInterface.java
@@ -0,0 +1,58 @@
+package com.bridgelabz.oceanfleet;
+
+import java.util.*;
+
+public class UserInterface {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        VesselUtil util = new VesselUtil();
+
+        System.out.println("Enter the number of vessels to be added");
+        int n = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Enter vessel details");
+
+        for (int i = 0; i < n; i++) {
+            String input = sc.nextLine();
+            String[] data = input.split(":");
+
+            String id = data[0];
+            String name = data[1];
+            double speed = Double.parseDouble(data[2]);
+            String type = data[3];
+
+            Vessel vessel = new Vessel(id, name, speed, type);
+            util.addVesselPerformance(vessel);
+        }
+
+        System.out.println("Enter the Vessel Id to check speed");
+        String searchId = sc.nextLine();
+
+        Vessel found = util.getVesselById(searchId);
+
+        if (found != null) {
+            System.out.println(found.getVesselId() + " | "
+                    + found.getVesselName() + " | "
+                    + found.getVesselType() + " | "
+                    + found.getAverageSpeed() + " knots");
+        } else {
+            System.out.println("Vessel Id " + searchId + " not found");
+        }
+
+        System.out.println("High performance vessels are");
+
+        List<Vessel> highList = util.getHighPerformanceVessels();
+
+        for (Vessel v : highList) {
+            System.out.println(v.getVesselId() + " | "
+                    + v.getVesselName() + " | "
+                    + v.getVesselType() + " | "
+                    + v.getAverageSpeed() + " knots");
+        }
+
+        sc.close();
+    }
+}
+
